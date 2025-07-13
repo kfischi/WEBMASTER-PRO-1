5740e38bef7f8eb7d2a47b3fee88f790996210a0
kfischi <kfir.biz@gmail.com>
2025-07-13 17:13:14 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,132 @@
+
+You reached the start of the range → Jul 13, 2025, 5:06 PM
+
+ 
+
+Node.js v22.11.0
+
+npm warn config production Use `--omit=dev` instead.
+
+ 
+
+> webmaster-pro-backend@3.0.0 start
+
+> node server.js
+
+ 
+
+node:internal/modules/cjs/loader:1252
+
+  throw err;
+
+  ^
+
+ 
+
+Error: Cannot find module '/app/server.js'
+
+    at Function._resolveFilename (node:internal/modules/cjs/loader:1249:15)
+
+    at Function._load (node:internal/modules/cjs/loader:1075:27)
+
+    at TracingChannel.traceSync (node:diagnostics_channel:315:14)
+
+    at wrapModuleLoad (node:internal/modules/cjs/loader:218:24)
+
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:170:5)
+
+    at node:internal/main/run_main_module:36:49 {
+
+  code: 'MODULE_NOT_FOUND',
+
+  requireStack: []
+
+}
+
+ 
+
+Node.js v22.11.0
+
+npm warn config production Use `--omit=dev` instead.
+
+ 
+
+> webmaster-pro-backend@3.0.0 start
+
+> node server.js
+
+ 
+
+node:internal/modules/cjs/loader:1252
+
+  throw err;
+
+  ^
+
+ 
+
+Error: Cannot find module '/app/server.js'
+
+    at Function._resolveFilename (node:internal/modules/cjs/loader:1249:15)
+
+    at Function._load (node:internal/modules/cjs/loader:1075:27)
+
+    at TracingChannel.traceSync (node:diagnostics_channel:315:14)
+
+    at wrapModuleLoad (node:internal/modules/cjs/loader:218:24)
+
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:170:5)
+
+    at node:internal/main/run_main_module:36:49 {
+
+  code: 'MODULE_NOT_FOUND',
+
+  requireStack: []
+
+}
+
+ 
+
+Node.js v22.11.0
+
+npm warn config production Use `--omit=dev` instead.
+
+ 
+
+> webmaster-pro-backend@3.0.0 start
+
+> node server.js
+
+ 
+
+node:internal/modules/cjs/loader:1252
+
+  throw err;
+
+  ^
+
+ 
+
+Error: Cannot find module '/app/server.js'
+
+    at Function._resolveFilename (node:internal/modules/cjs/loader:1249:15)
+
+    at Function._load (node:internal/modules/cjs/loader:1075:27)
+
+    at TracingChannel.traceSync (node:diagnostics_channel:315:14)
+
+    at wrapModuleLoad (node:internal/modules/cjs/loader:218:24)
+
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:170:5)
+
+    at node:internal/main/run_main_module:36:49 {
+
+  code: 'MODULE_NOT_FOUND',
+
+  requireStack: []
+
+}
+
+ 
+
+Node.js v22.11.0
